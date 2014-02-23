2947431baa2a53935a12495210ab1a2950f0dbed
nascenia <nascenia@nascenia-Inspiron-3521.(none)>
2014-02-23 17:32:10 +0600

    check cross browser widget

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vgifthost::Application.config.secret_token = 'bd00c5c258e2ef713c1577031c130f552a027aa12844def200241337d7a43aec479ded35b45e4e7f0936815e60182629cd223cce08104091a37472cfcbcf6c77'
